6c4a165d8a3f6a1796aaf36dde39d25efb76ed08
arud95 <antonrud95@yandex.ru>
2020-12-02 11:55:38 +0200

    config proj

---
+++ gatsby-config.js
@@ -0,0 +1,88 @@
+require('dotenv').config()
+
+const path = require('path')
+const {
+  GATSBY_ACTIVE_ENV,
+  GATSBY_SITE_URL,
+  GATSBY_TITLE,
+  GATSBY_DESCRIPTION,
+  GOOGLE_ANALYTICS_ID,
+} = process.env
+const isProd = GATSBY_ACTIVE_ENV === 'production'
+
+module.exports = {
+  siteMetadata: {
+    title: GATSBY_TITLE,
+    siteUrl: GATSBY_SITE_URL,
+    description: GATSBY_DESCRIPTION,
+    author: '@unikorns',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Poppins`,
+          `source sans pro\:400,500,600,700`, // you can also specify font weights and styles
+        ],
+        display: 'swap',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'assets',
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-alias-imports',
+      options: {
+        alias: {
+          '~': path.resolve(__dirname, 'src'),
+        },
+        extensions: [],
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'unikorns-gatsby-starter',
+        short_name: 'unikorns',
+        start_url: '/',
+        display: 'minimal-ui',
+        icon: 'src/assets/icons/global/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: GATSBY_SITE_URL,
+        sitemap: `${GATSBY_SITE_URL}/sitemap.xml`,
+        policy: isProd
+          ? [{ userAgent: '*', allow: '/' }]
+          : [{ userAgent: '*', disallow: '/' }],
+      },
+    },
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-sass',
+  ],
+}
