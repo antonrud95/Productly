f5eef68c9e3cae05908ee7960c1474a1a60462cc
arud95 <antonrud95@yandex.ru>
2020-12-05 12:48:00 +0200

    feature: add speach bubbles

--- src/components/sections/contacts-section/contacts-section.component.tsx
+++ src/components/sections/contacts-section/contacts-section.component.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { ContactsType } from '~/types/contacts.type'
+import SButton from '~/components/ui/general/button/button.component'
 import ContactsItem from '~/components/ui/general/contacts-item/contacts-item.component'
 import FC from '~/types/fc'
 
-// import styles from './contacts-section.module.scss'
+import styles from './contacts-section.module.scss'
 
 interface Props {
   contacts: ContactsType[]
@@ -13,6 +14,15 @@ interface Props {
 const ContactsSection: FC<Props> = ({ contacts }) => {
   return (
     <Container>
+      <div className={styles.contactsInfo}>
+        <h2>Need a super hero?</h2>
+        <p>
+          {' '}
+          Do you require some help for your project: Conception workshop,
+          prototyping, marketing strategy, landing page, Ux/UI?
+        </p>
+        <SButton variant="primary">Contact our expert</SButton>
+      </div>
       {contacts.map((contact: ContactsType, index) => {
         return (
           <ContactsItem
